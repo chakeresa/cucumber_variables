a198414207b82b53145504de5eae042b9c294991
Alexandra Chakeres <alexchakeres@gmail.com>
2019-11-29 11:39:55 -0700

    Add basic employee creation step

---
+++ features/step_definitions/step_argument_transformations.rb
@@ -0,0 +1,17 @@
+require './lib/employee'
+
+Given("the Employee") do |table|
+# table.column_names => ["var", "Name"]
+# table.hashes => [{"var"=>"E1", "name"=>"Bob"}]
+  table.hashes.each do |hash|
+    variable_name = hash["var"]
+    employee_name = hash["name"]
+
+    new_employee = Employee.new(name: employee_name)
+    expect(new_employee).to be_an(Employee)
+    expect(new_employee.name).to eq(employee_name)
+
+    @employees = Hash.new if @employees.class != Hash
+    @employees[variable_name] = new_employee
+  end
+end
