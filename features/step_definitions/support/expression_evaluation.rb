ef925c886d6c532d757b1bae5593f7ac8ed758cc
Alexandra Chakeres <alexchakeres@gmail.com>
2019-11-29 14:49:32 -0700

    Evaluate expressions without dots

--- features/step_definitions/support/expression_evaluation.rb
+++ features/step_definitions/support/expression_evaluation.rb
@@ -4,16 +4,19 @@ module ExpressionEvaluation
       expression.start_with?(key)
     end rescue nil
 
-    if variable_name && variable_name != expression
-      variable_value = @employees[variable_name]
+    if variable_name == expression
+      @employees[variable_name]
+    elsif variable_name != nil
       methods = expression.gsub(/^#{Regexp.quote(variable_name)}\.?/, '').split('.')
-      chain = variable_value
+      chain = @employees[variable_name]
 
       methods.each do |method|
         chain = chain.send(method)
       end
 
       chain
+    elsif expression == ''
+      nil
     else
       # eval(expression)
       expression
