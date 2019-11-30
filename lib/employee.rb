a847f9d64b115645b5206da4654d1cbfbf82a6ab
Alexandra Chakeres <alexchakeres@gmail.com>
2019-11-29 17:41:40 -0700

    Add defaults to Employee

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,8 +1,8 @@
 class Employee
   attr_accessor :name, :reports
 
-  def initialize(options)
-    @name = options[:name]
+  def initialize(options = {})
+    @name = options[:name] || 'default employee name'
     @reports = options[:reports] || []
   end
 end
